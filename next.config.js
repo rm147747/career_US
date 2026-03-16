a8962c2c4d22f772a0416ff86c25f2133cbd5bc2
Raphael Brandao <rb@firstsaude.com>
2026-03-16 01:38:48 -0300

    Fix formatting issues in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['react-markdown', 'remark-gfm']
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
